cf31f21c2fd8ba3f643d502e8697244b51782849
Hijikata1869 <liv.knk@gmail.com>
2026-09-09 21:09:08 +0900

    feat: UserのCRUDAPIを追加

--- workout-log-api/app/controllers/api/v1/users_controller.rb
+++ workout-log-api/app/controllers/api/v1/users_controller.rb
@@ -8,9 +8,33 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: user.safe_attributes, status: :ok
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_update_params)
+      render json: user.safe_attributes, status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head :no_content
+  end
+
   private
 
   def user_params
     params.expect(user: %i[nickname email password password_confirmation])
+  end
+
+  def user_update_params
+    params.expect(user: %i[nickname email])
   end
 end
